470e7639be5d6a6483e8957ea7d06d687f2109a8
Irischen666 <chenxiyuan8866@gmail.com>
2017-04-12 21:30:40 +0800

    correct the typing mistake of config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'jobs#index'
-  resources :admin do
+  namespace :admin do
     resources :jobs
   end
+
+  resources :jobs 
 end

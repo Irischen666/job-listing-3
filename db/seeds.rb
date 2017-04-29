cb66f54c7eb1928a19021b9ce652c051685297fc
陈颖君 <chenyingjun@chenyingjundeMacBook-Pro.local>
2017-04-29 17:51:12 +0800

    adjust innerpage and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: "test1@gmail.com", password: "1234567", password_confirmation: "1234567", is_admin: true)
+
+Job.create!(title: "Human Resources", description: "Thorough knowledge of applicable local, state and federal employment law.
+Strong talent acquisition skills.
+Ability to handle multiple priorities, make decisions, and resolve conflicts.
+Excellent communication and presentations skills.
+Ability to communicate effectively across all levels of the organization.
+Must be a team player with strong training and consultative skills.
+Proficient with various technologies such as Applicant Tracking Systems and", wage_lower_bound: 8000, wage_upper_bound: 10000, contact_email: "test@gmail.com",
+is_hidden: false)
+
+Job.create!(title: "Kindred Healthcare", description: "Thorough knowledge of applicable local, state and federal employment law.
+Strong talent acquisition skills.
+Ability to handle multiple priorities, make decisions, and resolve conflicts.
+Excellent communication and presentations skills.
+Ability to communicate effectively across all levels of the organization.
+Must be a team player with strong training and consultative skills.
+Proficient with various technologies such as Applicant Tracking Systems and", wage_lower_bound: 1000, wage_upper_bound: 2000, contact_email: "test@gmail.com",
+is_hidden: false)
